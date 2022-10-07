5b18fe178ad4dbcccae05a78c1fe160255f0e293
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-07 22:19:50 +0300

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_185839) do
+ActiveRecord::Schema.define(version: 2022_10_07_191701) do
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
